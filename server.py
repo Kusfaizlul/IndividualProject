8ab2e5172aae505d1bb43ddda1986ce958a3d70e
kusfaizlul <kuszocool@gmail.com>
2020-12-06 14:25:42 +0000

    Server

--- server.py
+++ server.py
@@ -5,19 +5,22 @@ HOST = '192.168.14.10'  # Host Address
 PORT = 4437               # Port Number
 
 with Soc.socket(Soc.AF_INET, Soc.SOCK_STREAM) as serv:
-        serv.bind((HOST, PORT))         # Binding
-        serv.listen()                   # Listen how many device
-        con, addr = serv.accept()       # Accept Client Con= IP , addr=random a>
-        with con:
-                print(' Connected by ',addr)
-                while True:
-                        data = con.recv(2000)   # Recv Data but in Byte
-                        if not data:
-                                break           # Error when data = null
-                        con.sendall(data)       # Send Msg to client in Byte
+	serv.bind((HOST, PORT))         # Binding
+	serv.listen()                   # Listen how many device
+	print ("\n Waiting for connecction ... ")
+	con, addr = serv.accept()       # Accept Client Con= IP , addr=random a>
+	with con:
+		print('\n Connected by ',addr)
+		while True:
+			data = con.recv(2000)   # Recv Data but in Byte
+			if not data:
+				break           # Error when data = null
+			con.send(data)	# Send Msg to client in Byte
 
-		choice = con.recv(1024)
-		choice = choice.decode()
+			#choice = con.recv(1024)
+			#choice = choice.decode()
 
-		msg = datetime.datetime.now()	#Get Date And Time
-		con.send(msg.encode())
+			msg = datetime.datetime.now()		#Get Date And Time
+			print("\n Time : " + str(msg))
+			con.send(str(msg).encode())
+
