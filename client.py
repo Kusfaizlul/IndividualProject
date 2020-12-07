b470196592bf7c4ba2f2990b817b85bf5f1700aa
kusfaizlul <kuszocool@gmail.com>
2020-12-07 12:03:32 +0000

    Updated Client

--- client.py
+++ client.py
@@ -1,8 +1,22 @@
-#from os import system
+from os import system
 import socket as soc
+from tqdm import tqdm 
+import time
+
+_ = system('clear')
+print ("\n Booting Up the Client App ... ")
+for i in tqdm (range (100),
+               desc="Loading…",
+               ascii=False, ncols=75):
+    time.sleep(0.02)
+
+print(" Complete !!!")
 
 HOST = '192.168.14.10'	# Host Address ( SERVER )
-PORT = 37		# Port Number 
+PORT = 4828		# Port Number
+
+print ("\n Connect to the server port .... ")
+_ = system('clear')
 
 with soc.socket(soc.AF_INET, soc.SOCK_STREAM) as s:
 	s.connect((HOST, PORT))			# Make a connection with server
